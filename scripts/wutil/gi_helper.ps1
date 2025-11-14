6d3fcab5039dfaa9d7f2a593f2cfcf78e123c12f
gryang <grant.yang@amd.com>
2025-11-14 12:59:41 -0500

    minor change.

--- scripts/wutil/gi_helper.ps1
+++ scripts/wutil/gi_helper.ps1
@@ -3,12 +3,11 @@
 
 . "$PSScriptRoot\tools.ps1"
 
-function Clone-GpuInterface {
-    python3 $PSScriptRoot/putil/clone_gi.py gpu_interface
+function Get-GpuInterface {
+    python $PSScriptRoot/../putil/clone_gi.py gpu_interface
 }
 
 function Build-GpuInterface {
-
     # To build gpu_interface with support for GFX10, GFX11, GFX11.5, GFX12:
     cmake -S . -B build_navi -G "Visual Studio 17 2022"
     cmake --build build_navi --config Release
